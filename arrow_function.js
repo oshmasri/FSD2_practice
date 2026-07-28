2ec1922114c24d3c3e832d7e248ccf0278f05d26
oshmasri <oshmasrisammeta@gmail.com>
2026-07-28 21:05:05 +0530

    Initial commit

--- arrow_function.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-//Arrow function using map builtin array method.
-let salary = [100, 200, 700, 900, 500];
-let double_salary = salary.map(num => num * 2);
-console.log(double_salary);
-//reduce built-in array method.
-let total_salary = salary.reduce((current, total) => current + total, 0);
-console.log(total_salary);
